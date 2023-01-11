5dd15db1c16c185ab179159a1c260bb6fa7d7a6b
gilles <gillesdejong@icloud.com>
2023-01-11 11:55:31 +0100

    test

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,20 @@
+
+
+<html>
+<head>
+<title>Gilles' testpagina</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+
 <?php
-echo 'Base page';
+echo 'Base page hello';
 echo '</br> See <a href="info.php">info</a> for install info';
 echo '</br> See <a href="table.php">table</a> for an SQL table';
+echo 5 * 3; ?>
+
+</body>
+</html>
