7130c83a61eba399226536832246d436a7f6c7b3
jesse <jesseoomen2002@gmail.com>
2023-01-11 11:53:42 +0100

    initial branch

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
 <?php
-echo 'Base page';
+echo 'Base page hello jesse';
 echo '</br> See <a href="info.php">info</a> for install info';
 echo '</br> See <a href="table.php">table</a> for an SQL table';
+echo 5 * 3;
+?>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
