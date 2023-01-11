a06b970706be3f629905f81f5eb2b8c78f68aa77
Ruben Nigel Veltman <rubennigelveltman@gmail.com>
2023-01-11 12:38:36 +0100

    Dropdown functionality and testing

--- pages/voorbeeld.php
+++ pages/voorbeeld.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>Voorbeeld</title>
+    <link rel="stylesheet" href="global.css" type="text/css"/>
 </head>
 
 <body>
@@ -13,7 +14,10 @@
     echo '<h1> Dit is een voorbeeld </h1>';
     echo '<p> Dit is de subtext </p>';
     require '../components/dropdown_function.php';
-    writeMsg("Hallo");
+
+    $links = array("Test 1"=>"test_1.php", "Test 2"=>"test_2.php", "Test 3"=>"test_3.php");
+
+    dropDown($links);
 
     ?>
 
