87f784dab7c14b085541f5a3328928c9b4edb7a7
gilles <gillesdejong@icloud.com>
2023-01-11 14:44:40 +0100

    voor jesse

---
+++ pages/home.php
@@ -0,0 +1,24 @@
+
+
+<html>
+<head>
+    <link rel="stylesheet" href="global.css" type="text/css">
+    <title>Gilles' testpagina</title>
+</head>
+<body>
+
+<h1>My </h1>
+<p>My first paragraph. <br></p>
+    <img src='pictures/icon1.png' alt='Foto icon' style='width:100px;height:100px;'>
+    <img src='pictures/icon2.png' alt='Foto icon' style='width:100px;height:100px;'>
+<a href='../~gillesj/home.php'>blabal</a>
+
+
+
+<?php
+require_once '../components/topbar.php';
+maketext();
+?>
+
+</body>
+</html>
--- pages/index.php
+++ pages/index.php
@@ -2,19 +2,12 @@
 
 <html>
 <head>
-<title>Gilles' testpagina</title>
+    <link rel="stylesheet" href="global.css" type="text/css">
+    <title>Gilles' testpagina</title>
 </head>
 <body>
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-
-
-<?php
-echo 'Base page hello';
-echo '</br> See <a href="info.php">info</a> for install info';
-echo '</br> See <a href="table.php">table</a> for an SQL table';
-echo 5 * 3; ?>
+<h1>hahaha gepranked</h1>
 
 </body>
 </html>
