c3da409085bd2569747ef9d60331887817401bce
gilles <gillesdejong@icloud.com>
2023-01-12 10:25:41 +0100

    Creating topbar (wip)

--- pages/home.php
+++ pages/home.php
@@ -2,16 +2,16 @@
 
 <html>
 <head>
-    <link rel="stylesheet" href="global.css" type="text/css">
+    <link rel="stylesheet" href="styles/global.css" type="text/css">
     <title>Gilles' testpagina</title>
 </head>
 <body>
 
 <h1>My </h1>
-<p>My first paragraph. <br></p>
+<p>My first. <br></p>
     <img src='pictures/icon1.png' alt='Foto icon' style='width:100px;height:100px;'>
     <img src='pictures/icon2.png' alt='Foto icon' style='width:100px;height:100px;'>
-<a href='home.php'>blabal</a>
+<a href='home.php'>bloep</a>
 
 
 
