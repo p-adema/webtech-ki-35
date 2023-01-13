36c30643f4ad7da2b6556c79cce51c9ee19e5765
p adema <117938385+p-adema@users.noreply.github.com>
2023-01-13 10:04:14 +0100

    Authentication API update

--- components/api_resolve.php
+++ components/api_resolve.php
@@ -4,21 +4,21 @@
 # as these functions end script execution
 use JetBrains\PhpStorm\NoReturn;
 
-#[NoReturn] function api_fail(array $errors, string $failure_msg, array $data = []): void
+#[NoReturn] function api_fail(string $message, array $errors, array $data = []): void
 {
     $response['success'] = false;
-    $response['message'] = $failure_msg;
+    $response['message'] = $message;
     $response['errors'] = $errors;
     $response['data'] = $data;
     echo json_encode($response);
     die();
 }
 
-#[NoReturn] function api_succeed(string $success_msg, array $data = []): void
+#[NoReturn] function api_succeed(string $message, array $errors = [], array $data = []): void
 {
     $response['success'] = true;
-    $response['message'] = $success_msg;
-    $response['errors'] = [];
+    $response['message'] = $message;
+    $response['errors'] = $errors;
     $response['data'] = $data;
     echo json_encode($response);
     die();
