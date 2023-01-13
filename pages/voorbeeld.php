b91cd5314cb6c05c4feb54dc1dd17d7ab40fa56c
Ruben Nigel Veltman <rubennigelveltman@gmail.com>
2023-01-13 09:56:52 +0100

    Kleine test

--- pages/voorbeeld.php
+++ pages/voorbeeld.php
@@ -14,23 +14,14 @@
     echo '<h1> Dit is een voorbeeld </h1>';
     echo '<p> Dit is de subtext </p>';
     require '../components/dropdown_function.php';
+    require '../components/sidebar_function.php';
 
     $links = array("Test 1"=>"test_1.php", "Test 2"=>"test_2.php", "Test 3"=>"test_3.php");
 
     dropDown($links);
+    sidebar($links);
 
     ?>
-
-    <html>
-    <body>
-
-    <video width="320" height="240" controls>
-        <source src="test.mp4" type="video/mp4">
-        <source src="test.ogg" type="video/ogg">
-    </video>
-
-    </body>
-    </html>
 
 </body>
 </html>
