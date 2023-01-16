933fd5966f80745669c8d40d01c4f351882ab363
p adema <117938385+p-adema@users.noreply.github.com>
2023-01-16 18:24:19 +0100

    Require email verification before login

--- pages/api/login.php
+++ pages/api/login.php
@@ -35,9 +35,9 @@ $data = [
 ];
 
 if (filter_var($name, FILTER_VALIDATE_EMAIL)) {
-    $sql = 'SELECT (password) FROM db.users WHERE (email = :name);';
+    $sql = 'SELECT password, verified FROM db.users WHERE (email = :name);';
 } else {
-    $sql = 'SELECT (password) FROM db.users WHERE (name = :name);';
+    $sql = 'SELECT password, verified FROM db.users WHERE (name = :name);';
 }
 
 require "pdo_read.php";
@@ -53,11 +53,16 @@ if (!$sql_prep->execute($data)) {
     $errors['submit'][] = 'Internal server error';
     api_fail('Internal server error, try again later', $errors);
 }
-$hash = $sql_prep->fetch();
-if (empty($hash) or !password_verify($password, $hash[0])) {
+$user = $sql_prep->fetch();
+if (empty($user) or !password_verify($password, $user['password'])) {
     $errors['submit'][] = 'Incorrect username or password';
     api_fail('Username or password invalid', $errors);
 }
+if (!$user['verified']) {
+    $errors['submit'][] = 'Please verify your email first';
+    api_fail('Unverified email address', $errors);
+}
+
 if (!api_login($name)) {
     $errors['submit'][] = 'Unexpected server error';
     api_fail('Unexpected server error', $errors);
