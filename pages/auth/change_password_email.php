0ef593a2594d843328f6657a4ddce2a224b217d8
p adema <117938385+p-adema@users.noreply.github.com>
2023-01-16 09:22:37 +0100

    Fix form-content div

--- pages/auth/change_password_email.php
+++ pages/auth/change_password_email.php
@@ -2,13 +2,13 @@
 require 'html_page.php';
 html_header(title: 'Change password', styled: 'form.css', scripted: false);
 ?>
-    <body>
-    <div class="form_content">
-        <h1> Change password </h1>
-        <div class="form-outline">
-            <form action="/api/change_password_email.php" method="POST">
-                <p> Fill in your new password bellow.</p>
-                <?php
+<body>
+<div class="form-content">
+    <h1> Change password </h1>
+    <div class="form-outline">
+        <form action="/api/change_password_email.php" method="POST">
+            <p> Fill in your new password bellow.</p>
+            <?php
                 require "form_elements.php";
                 form_input('password', 'Password', type: 'password', input_attrs: "autocomplete=\"new-password\"");
                 form_input('password_repeated', 'Repeat Password', type: 'password', input_attrs: "autocomplete=\"new-password\"");
