98e95bc3b4107d8712c8bdb36169ab99562eeffd
Ruben Nigel Veltman <rubennigelveltman@gmail.com>
2023-01-16 11:28:21 +0100

    Bank functies en een deel van de opmaak

--- pages/bank/index.php
+++ pages/bank/index.php
@@ -3,14 +3,37 @@ require 'html_page.php';
 html_header(title: 'balance', styled: true);
 ?>
 <div id="main-container">
-    <div id="big-balance-box">
-        <div id="balance">
-            Dit is je balans: €
+    <div class="big-balance-box">
+        <div class="balance">
             <?php
-            require "bank.php";
-            $user_id = 2;
+            require "bank_functionality.php";
+            $user_id = 1;
+            echo 'Dit is uw balans: €';
             echo get_balance($user_id);
             ?>
+        </div>
+    </div>
+    <div class="main_pending_box">
+        <div class="pending_box">
+            <?php
+            $all_current_transactions = get_pending_transaction($user_id);
+            foreach ($all_current_transactions as $pending_transaction) {
+                print_pending($pending_transaction);
+                echo "<br>";
+            }
+            ?>
+        </div>
+    </div>
+    <div class="all-transactions">
+        <div class="transaction-box">
+            <div class="transaction-text">
+                <?php
+                $past_transactions = get_transaction_log($user_id);
+                foreach ($past_transactions as $transaction) {
+                    print_transaction($transaction);
+                }
+                ?>
+            </div>
         </div>
     </div>
 </div>
