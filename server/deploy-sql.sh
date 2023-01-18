f0eb4353774d7ecfa25a507f2b9a91739840d1f5
p adema <117938385+p-adema@users.noreply.github.com>
2023-01-18 11:37:32 +0100

    Added initial data to deploy-sql.sh

--- server/deploy-sql.sh
+++ server/deploy-sql.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rsync ./server/schema.sql -az "$1"@webtech-ki35.webtech-uva.nl:/var/www/server/schema.sql
 rsync ./server/users.sh -az "$1"@webtech-ki35.webtech-uva.nl:/var/www/server/users.sh
+rsync ./server/initial_data.sql -az "$1"@webtech-ki35.webtech-uva.nl:/var/www/server/initial_data.sql
 rsync ./server/client-sql.sh -az "$1"@webtech-ki35.webtech-uva.nl:/var/www/server/client-sql.sh
 ssh "$1"@webtech-ki35.webtech-uva.nl bash /var/www/server/client-sql.sh
