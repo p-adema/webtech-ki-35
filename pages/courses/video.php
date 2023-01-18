0504eceb7416f76da31d9d8a641f05530de46bc3
Ruben Nigel Veltman <rubennigelveltman@gmail.com>
2023-01-18 12:45:48 +0100

    Video-tag check and basic video functionality

--- pages/courses/video.php
+++ pages/courses/video.php
@@ -1,19 +1,25 @@
 <?php
 require 'html_page.php';
+require 'video_functionality.php';
 html_header(title: 'Video', styled: true, scripted: false);
 
-?>
+$tag = $_GET['tag'];
+$video_info = get_video_data($tag);
 
+if (isset($_GET['tag']) and $video_info !== false): ?>
     <body>
     <div class="video-outline">
         <div class="video">
             <video width="600" controls>
-                <source src="/videos/example.mp4" type="video/mp4">
+                <source src="/videos/<?php echo $tag; ?>.mp4" type="video/mp4">
                 Your browser does not support HTML video.
             </video>
         </div>
     </div>
     </body>
+    <?php
 
-<?php
+else:?> <span>This link doesn't seem quite right.</span>
+<?php endif;
+
 html_footer();
