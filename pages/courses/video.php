6ba4700557a3ac9b11445ff101a36649e8193d8f
Ruben Nigel Veltman <rubennigelveltman@gmail.com>
2023-01-18 11:18:32 +0100

    Video tests

--- pages/courses/video.php
+++ pages/courses/video.php
@@ -4,8 +4,16 @@ html_header(title: 'Video', styled: true, scripted: false);
 
 ?>
 
-<body>
-<video></video>
-</body>
+    <body>
+    <div class="video-outline">
+        <div class="video">
+            <video width="600" controls>
+                <source src="/videos/example.mp4" type="video/mp4">
+                Your browser does not support HTML video.
+            </video>
+        </div>
+    </div>
+    </body>
 
+<?php
 html_footer();
