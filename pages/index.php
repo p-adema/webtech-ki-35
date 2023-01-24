6f42c5e596800702666ef8ab545a5921fefc6c6d
p adema <117938385+p-adema@users.noreply.github.com>
2023-01-24 21:38:52 +0100

    Remove old example files

--- pages/index.php
+++ pages/index.php
@@ -4,11 +4,9 @@ html_header('EduGrove', scripted: 'ajax');
 
 echo '<p>';
 echo 'Base page';
-echo '<br/> See <a href="/info">info</a> for install info';
 echo '<br/> See <a href="/auth/register">auth/register</a> for a registration form';
 echo '<br/> See <a href="/auth/login">auth/login</a> for a login form';
 echo '<br/> See <a href="/auth/logout">auth/logout</a> for a logout form';
-echo '<br/> See <a href="/session">session</a> for a session test';
 echo '<br/> See <a href="/auth/forgot_password">forgot password</a> in case you forgot your password';
 echo '<br/> See <a href="/modify_cart">modify cart</a> to add/remove example to/from cart';
 echo '<br/> See <a href="/show_cart">show cart</a> to view cart';
